7f3fbc39736cd414031169f09f72e97ebb0e41df
John Kastner <john.h.kastner@gmail.com>
2020-02-24 13:33:32 -0500

    Add loading indicators

--- js_buzz.js
+++ js_buzz.js
@@ -6,6 +6,7 @@ var tiles = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 
 var markers = L.markerClusterGroup({
     chunkedLoading: true,
+    chunkProgress: updateProgressBar,
     iconCreateFunction: function(cluster) {
         var childCount = cluster.getAllChildMarkers().reduce((a,v) => a + v.count, 0)
         return markerIcon(childCount);
@@ -177,6 +178,9 @@ function epochMinsToDate(mins) {
 
 function updateMap() {
     terminateAnimation();
+    var loader = document.getElementById('loader');
+    loader.style.display = 'block';
+
     var url = constructQueryURL();
     var xhr = new XMLHttpRequest();
     xhr.open("GET", url, true);
@@ -188,7 +192,25 @@ function updateMap() {
         } else {
           console.error(xhr.statusText);
         }
+        loader.style.display = 'none';
       }
     }
     xhr.send(null);
+}
+
+
+function updateProgressBar(processed, total, elapsed, layersArray) {
+    var progress = document.getElementById('progress');
+    var progressBar = document.getElementById('progress-bar');
+
+    if (elapsed > 500) {
+        // if it takes more than half a second to load, display the progress bar:
+        progress.style.display = 'block';
+        progressBar.style.width = Math.round(processed/total*100) + '%';
+    }
+
+    if (processed === total) {
+        // all markers processed - hide the progress bar:
+        progress.style.display = 'none';
+    }
 }
