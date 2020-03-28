86ea0df82fbbdbf45212d66576e6aa7461e6971b
John Kastner <john.h.kastner@gmail.com>
2020-03-28 16:38:05 -0400

    Add twitter data

--- webpage/js_buzz.js
+++ webpage/js_buzz.js
@@ -11,19 +11,6 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
     id: 'mapbox.streets',
 }).addTo(map);
 
-var markers = L.markerClusterGroup({
-    chunkedLoading: true,
-    chunkProgress: updateProgressBar,
-    iconCreateFunction: function(cluster) {
-        var childCount = cluster.getAllChildMarkers().reduce((a,v) => a + v.count, 0)
-        return markerIcon(childCount);
-    }
-});
-markers.on('spiderfied', function (a) {
-    var allArticles = a.markers.flatMap( function (m) {return m.articles});
-    L.popup({maxHeight: 200}).setLatLng(a.cluster.getLatLng()).setContent(makePopupHtml(allArticles, a.markers[0].name)).openOn(map);
-});
-
 $( function() {
   $( "#slider-range" ).slider({
     range: true,
@@ -42,17 +29,152 @@ $( function() {
   });
 } );
 
-var markerList;
-var nodeList;
-
 var dataStartDate;
 var dataEndDate;
-
-updateMap();
 
 var animateWindow = 7 * 24 * 60;
 var animateStep = 24 * 60;
 var animateSpeed = 100;
+
+class NewsStandDataLayer {
+    constructor(plottingLayer, color_fn, url_fn) {
+        var that = this;
+        this.markers = L.markerClusterGroup({
+            chunkedLoading: true,
+            chunkProgress: updateProgressBar,
+            iconCreateFunction: function(cluster) {
+                var childCount = cluster.getAllChildMarkers().reduce((a,v) => a + v.count, 0)
+                return that.markerIcon(childCount);
+            }
+        });
+        this.markers.on('spiderfied', function (a) {
+            var allArticles = a.markers.flatMap( function (m) {return m.articles});
+            L.popup({maxHeight: 200}).setLatLng(a.cluster.getLatLng()).setContent(that.makePopupHtml(allArticles, a.markers[0].name)).openOn(map);
+        });
+        map.addLayer(this.markers);
+
+        this.plottingLayer = plottingLayer;
+        this.color_fn = color_fn;
+        this.url_fn = url_fn;
+        this.markerList = [];
+        this.node_list = [];
+    }
+
+    togglePlotting() {
+        this.plottingLayer = ! this.plottingLayer;
+
+        if(this.plottingLayer){
+            var startDate = dateToEpochMins(document.getElementById("display_start_date").valueAsDate);
+            var endDate = dateToEpochMins(document.getElementById("display_end_date").valueAsDate);
+            var subMarkerList = this.markersBetween(startDate, endDate);
+            this.markers.clearLayers();
+            this.markers.addLayers(subMarkerList);
+        } else {
+            this.markers.clearLayers();
+        }
+    }
+
+    markersBetween(timeStart, timeEnd) {
+        var iStart = nodeIndexOfTime(this.nodeList, timeStart)
+        var iEnd = nodeIndexOfTime(this.nodeList, timeEnd)
+        return this.markerList.slice(iStart, iEnd);
+    }
+
+    setMarkers(nodes) {
+        this.markers.clearLayers();
+        this.nodeList = nodes;
+        var that = this;
+        this.markerList = this.nodeList.map(function (p) {
+            var marker = new L.Marker(L.latLng(p.lat, p.lng), { icon: that.markerIcon(p.count)});
+            var articles;
+            if (p.articles) {
+                articles = JSON.parse(p.articles);
+                articles = articles.map(function (a) {
+                    return {title: a['f1'], url: a['f2']};
+                });
+            } else {
+                articles = [];
+            }
+            marker.bindPopup(that.makePopupHtml(articles, p.name),{maxHeight: 100});
+            marker.count = p.count;
+            marker.name = p.name;
+            marker.articles = articles;
+            return marker;
+        });
+        if(this.plottingLayer){
+          this.markers.addLayers(this.markerList);
+        }
+
+        if(this.nodeList.length > 0) {
+          var min = this.nodeList[0].time;
+          var max = this.nodeList[this.nodeList.length - 1].time;
+
+          dataStartDate = epochMinsToDate(min);
+          dataEndDate = epochMinsToDate(max)
+
+          document.getElementById("start_date").valueAsDate = dataStartDate;
+          document.getElementById("end_date").valueAsDate = dataEndDate;
+          $("#slider-range").slider("option", "min", min);
+          $("#slider-range").slider("option", "max", max);
+
+          setDisplayedDateRange(min, max);
+        }
+    }
+
+    updateLayer() {
+        terminateAnimation();
+        var loader = document.getElementById('loader');
+        loader.style.display = 'block';
+
+        var url = this.url_fn();
+        var xhr = new XMLHttpRequest();
+        var that = this;
+        xhr.open("GET", url, true);
+        xhr.onload = function (e) {
+          if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+              var nodes = JSON.parse(xhr.responseText);
+              that.setMarkers(nodes);
+            } else {
+              console.error(xhr.statusText);
+            }
+            loader.style.display = 'none';
+          }
+        }
+        xhr.send(null);
+    }
+
+    makePopupHtml(articles, name) {
+        var articles_html = articles.map(function(e) {
+          return "<li><a href=" + e.url +">" + e.title + "</a></li>";
+        }).join("");
+        return "<em>"+name+"</em><br><ol>"+articles_html+"</ol>";
+    }
+
+    markerIcon(clusterSize) {
+        var size = markerSize(clusterSize);
+        var color = this.color_fn(clusterSize);
+
+        var elemStyle =
+          'border-radius: 50%;' +
+          'width: '  + size + 'px;' +
+          'height: ' + size + 'px;' +
+          'line-height: ' + size + 'px;' +
+          'font-weight: bold;' +
+          'background-color: ' + color + ';';
+
+        return new L.DivIcon({ html: '<div style="' + elemStyle + '">' + clusterSize + '</div>', className: 'marker-cluster', iconSize: new L.Point(size, size) });
+    }
+
+    plotData(timeStart, timeEnd){
+        if(this.plottingLayer){
+            var subMarkerList = this.markersBetween(timeStart, timeEnd);
+            this.markers.clearLayers();
+            this.markers.addLayers(subMarkerList);
+        }
+    }
+
+}
 
 class JHUDataLayer {
     constructor(color, timeSeries, plottingLayer) {
@@ -60,7 +182,7 @@ class JHUDataLayer {
         this.timeSeries = timeSeries;
         this.plottingLayer = plottingLayer;
 
-        var temp = this;
+        var that = this;
         this.markers = L.markerClusterGroup({
             chunkedLoading: true,
             showCoverageOnHover: false,
@@ -68,7 +190,7 @@ class JHUDataLayer {
             chunkProgress: updateProgressBar,
             iconCreateFunction: function(cluster) {
                 var childCount = cluster.getAllChildMarkers().reduce((a,v) => a + v.count, 0)
-                return temp.layerIcon(childCount);
+                return that.layerIcon(childCount);
             }
         });
         map.addLayer(this.markers);
@@ -87,13 +209,13 @@ class JHUDataLayer {
 
     plotData(timeStart, timeEnd){
         if( this.plottingLayer ){
-            var temp = this;
+            var that = this;
             var timeSeriesMarkers = this.timeSeries.map(function (p) {
                 var indexStart = nodeIndexOfTime(p.time_series, timeStart);
                 var indexEnd = nodeIndexOfTime(p.time_series, timeEnd);
                 var count =  p.time_series[indexEnd].cases - p.time_series[indexStart].cases;
 
-                var icon = temp.layerIcon(count);
+                var icon = that.layerIcon(count);
                 var marker = L.marker([p.lat, p.lng], {icon: icon});
                 marker.count = count;
                 return marker;
@@ -137,19 +259,57 @@ var recoveredSelected = document.getElementById("recovered_checkbox").checked;
 var recoveredLayer = new JHUDataLayer('green', timeSeriesRecovered, recoveredSelected);
 
 var newsDataSelected = document.getElementById("news_data_checkbox").checked;
-function toggleNewsData() {
-    newsDataSelected = ! newsDataSelected;
+var newsLayer = new NewsStandDataLayer(newsDataSelected,
+    function(clusterSize){
+        var color;
+        if (clusterSize < 10) {
+            color = 'rgba(181, 226, 140, 0.6)';
+        } else if (clusterSize < 100) {
+            color = 'rgba(241, 211, 87, 0.6)';
+        } else {
+            color = 'rgba(253, 156, 115, 0.6)';
+        }
+        return color;
+    },
+    function() {
+        var keyword = document.getElementById('keyword').value;
+        var start_date = new Date(document.getElementById('start_date').value);
+        var end_date = new Date(document.getElementById('end_date').value);
 
-    if(newsDataSelected){
-        var startDate = dateToEpochMins(document.getElementById("display_start_date").valueAsDate);
-        var endDate = dateToEpochMins(document.getElementById("display_end_date").valueAsDate);
-        var subMarkerList = markersBetween(startDate, endDate);
-        markers.clearLayers();
-        markers.addLayers(subMarkerList);
-    } else {
-        markers.clearLayers();
-    }
-}
+        var start_epoch_mins = dateToEpochMins(start_date);
+        var end_epoch_mins = dateToEpochMins(end_date) + (1000 * 60) - 1;
+
+        var url = "https://newsstand.umiacs.umd.edu/coronaviz/disease_time_query" +
+              "?keyword=" + keyword +
+              "&start_date=" + start_epoch_mins +
+              "&end_date=" + end_epoch_mins;
+
+        return url;
+    });
+
+var twitterDataSelected = document.getElementById("twitter_data_checkbox").checked;
+var twitterLayer = new NewsStandDataLayer(twitterDataSelected,
+    function(clusterSize){
+        return color = 'rgba(0, 0, 255, 0.6)';
+    },
+    function() {
+      var start_date = new Date(document.getElementById('start_date').value);
+      var end_date = new Date(document.getElementById('end_date').value);
+
+      var start_epoch_mins = dateToEpochMins(start_date);
+      var end_epoch_mins = dateToEpochMins(end_date) + (1000 * 60) - 1;
+
+      var url = "https://newsstand.umiacs.umd.edu/coronaviz/twitter_query" +
+            "?start_date=" + start_epoch_mins +
+            "&end_date=" + end_epoch_mins;
+
+      return url;
+    });
+
+var dataLayers = [confirmedLayer, deathsLayer, recoveredLayer, newsLayer, twitterLayer];
+
+newsLayer.updateLayer();
+twitterLayer.updateLayer();
 
 //TODO: make this a binary search since that's definitely more efficient. To bad
 // I'm too lazy to do it right the first time. Well, it seems to work as is,
@@ -163,55 +323,6 @@ function nodeIndexOfTime(list, time) {
     } else {
         return index;
     }
-}
-
-function markersBetween(timeStart, timeEnd) {
-    var iStart = nodeIndexOfTime(nodeList, timeStart)
-    var iEnd = nodeIndexOfTime(nodeList, timeEnd)
-    return markerList.slice(iStart, iEnd);
-}
-
-function setMarkers(nodes) {
-  markers.clearLayers();
-  nodeList = nodes;
-  markerList = nodeList.map(function (p) {
-      var marker = new L.Marker(L.latLng(p.lat, p.lng), { icon: markerIcon(p.count)});
-      var articles = JSON.parse(p.articles);
-      articles = articles.map(function (a) {
-          return {title: a['f1'], url: a['f2']};
-      });
-      marker.bindPopup(makePopupHtml(articles, p.name),{maxHeight: 100});
-      marker.count = p.count;
-      marker.name = p.name;
-      marker.articles = articles;
-      return marker;
-  });
-  if(newsDataSelected){
-    markers.addLayers(markerList);
-  }
-  map.addLayer(markers);
-
-  if(nodeList.length > 0) {
-    var min = nodeList[0].time;
-    var max = nodeList[nodeList.length - 1].time;
-
-    dataStartDate = epochMinsToDate(min);
-    dataEndDate = epochMinsToDate(max)
-
-    document.getElementById("start_date").valueAsDate = dataStartDate;
-    document.getElementById("end_date").valueAsDate = dataEndDate;
-    $("#slider-range").slider("option", "min", min);
-    $("#slider-range").slider("option", "max", max);
-
-    setDisplayedDateRange(min, max);
-  }
-}
-
-function makePopupHtml(articles, name) {
-  var articles_html = articles.map(function(e) {
-    return "<li><a href=" + e.url +">" + e.title + "</a></li>";
-  }).join("");
-  return "<em>"+name+"</em><br><ol>"+articles_html+"</ol>";
 }
 
 // 99% sure this isn't the correct way to do this, but I can't be bothered to
@@ -238,51 +349,8 @@ function terminateAnimation() {
     document.getElementById("animate").innerHTML = 'Start Animation &raquo;';
 }
 
-function markerIcon(clusterSize) {
-  var size = markerSize(clusterSize);
-  var color = markerColor(clusterSize);
-
-  var elemStyle =
-    'border-radius: 50%;' +
-    'width: '  + size + 'px;' +
-    'height: ' + size + 'px;' +
-    'line-height: ' + size + 'px;' +
-    'font-weight: bold;' +
-    'background-color: ' + color + ';';
-
-  return new L.DivIcon({ html: '<div style="' + elemStyle + '">' + clusterSize + '</div>', className: 'marker-cluster', iconSize: new L.Point(size, size) });
-}
-
 function markerSize(clusterSize) {
   return 20 + Math.log(clusterSize)**2;
-}
-
-function markerColor(clusterSize) {
-  var color;
-  if (clusterSize < 10) {
-      color = 'rgba(181, 226, 140, 0.6)';
-  } else if (clusterSize < 100) {
-      color = 'rgba(241, 211, 87, 0.6)';
-  } else {
-      color = 'rgba(253, 156, 115, 0.6)';
-  }
-  return color;
-}
-
-function constructQueryURL() {
-  var keyword = document.getElementById('keyword').value;
-  var start_date = new Date(document.getElementById('start_date').value);
-  var end_date = new Date(document.getElementById('end_date').value);
-
-  var start_epoch_mins = dateToEpochMins(start_date);
-  var end_epoch_mins = dateToEpochMins(end_date) + (1000 * 60) - 1;
-
-  var url = "https://newsstand.umiacs.umd.edu/coronaviz/disease_time_query" +
-        "?keyword=" + keyword +
-        "&start_date=" + start_epoch_mins +
-        "&end_date=" + end_epoch_mins;
-
-  return url;
 }
 
 function dateToEpochMins(date) {
@@ -291,28 +359,6 @@ function dateToEpochMins(date) {
 
 function epochMinsToDate(mins) {
     return new Date(mins * 60 * 1000);
-}
-
-function updateMap() {
-    terminateAnimation();
-    var loader = document.getElementById('loader');
-    loader.style.display = 'block';
-
-    var url = constructQueryURL();
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
-    xhr.onload = function (e) {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-          var nodes = JSON.parse(xhr.responseText);
-          setMarkers(nodes);
-        } else {
-          console.error(xhr.statusText);
-        }
-        loader.style.display = 'none';
-      }
-    }
-    xhr.send(null);
 }
 
 function updateProgressBar(processed, total, elapsed, layersArray) {
@@ -346,14 +392,9 @@ function setDisplayedDateRange(startMins, endMins) {
     document.getElementById("display_end_date").valueAsDate = epochMinsToDate(endMins);
     $("#slider-range").slider("values", [startMins, endMins]);
 
-    // Update news data layer if applicable
-    if (newsDataSelected) {
-      var subMarkerList = markersBetween(startMins, endMins);
-      markers.clearLayers();
-      markers.addLayers(subMarkerList);
-    }
-
-    // Update JHU data layers
+    // Update all layeres for new range
+    newsLayer.plotData(startMins, endMins);
+    twitterLayer.plotData(startMins, endMins);
     confirmedLayer.plotData(startMins, endMins);
     deathsLayer.plotData(startMins, endMins);
     recoveredLayer.plotData(startMins, endMins);
