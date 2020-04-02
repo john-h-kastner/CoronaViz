e38490f1a205ff2be17949060dd2c11ae6afe790
John Kastner <john.h.kastner@gmail.com>
2020-04-02 12:50:52 -0400

    Update for new US data format

--- Makefile
+++ Makefile
@@ -12,8 +12,8 @@ DATA_NAMES = $(addprefix time_series_19-covid-,$(DATA_TYPES))
 JSON_FILES = $(addprefix $(WEBPAGE_DIR)/,$(addsuffix .js,$(DATA_NAMES)))
 
 define JS_template =
-$(WEBPAGE_DIR)/time_series_19-covid-$(1).js: $(JHU_DATA_DIR)/time_series_covid19_$(1)_global.csv convert_data.py
-	python convert_data.py $$< $$@ timeSeries$(1)
+$(WEBPAGE_DIR)/time_series_19-covid-$(1).js: convert_data.py
+	python convert_data.py $(JHU_DATA_DIR)/time_series_covid19_$(1)_global.csv $(JHU_DATA_DIR)/time_series_covid19_$(1)_US.csv $$@ timeSeries$(1)
 endef
 
 $(foreach type,$(DATA_TYPES),$(eval $(call JS_template,$(type))))
--- convert_data.py
+++ convert_data.py
@@ -35,19 +35,66 @@ def convert_row(row_dict):
   else:
       return None
 
+def convert_row_us(row_dict):
+  location = row_dict["Combined_Key"]
+
+  lat = row_dict["Lat"]
+  lng = row_dict["Long_"]
+
+  row_dict.pop("Combined_Key", None)
+  row_dict.pop("Province_State", None)
+  row_dict.pop("Country_Region", None)
+  row_dict.pop("Lat", None)
+  row_dict.pop("Long_", None)
+  row_dict.pop("UID", None)
+  row_dict.pop("iso2", None)
+  row_dict.pop("iso3", None)
+  row_dict.pop("FIPS", None)
+  row_dict.pop("Admin2", None)
+  row_dict.pop("code3", None)
+  row_dict.pop("Population", None)
+
+  time_series_list = []
+  for date_str, cases_str in row_dict.items():
+      cases = int(cases_str)
+      if cases > 0:
+        date = parse(date_str)
+        epoch_mins = date.timestamp() / 60;
+        time_series_list.append({'time': epoch_mins, 'cases': cases})
+
+  if time_series_list:
+      return { 'name': location,
+               'lat': lat,
+               'lng': lng,
+               'time_series': time_series_list}
+  else:
+      return None
+
+
 parser = argparse.ArgumentParser()
-parser.add_argument('CSV_FILE', help='Input CSV file')
+parser.add_argument('GLOBE_CSV_FILE', help='Input global CSV file')
+parser.add_argument('US_CSV_FILE', help='Input US CSV file')
 parser.add_argument('JS_FILE', help='Output JS file for JSON data')
 parser.add_argument('JS_VAR', help='Variable name in JS file')
 args = parser.parse_args()
 
 row_list = []
-with open(args.CSV_FILE, 'r') as csv_file:
+with open(args.GLOBE_CSV_FILE, 'r') as csv_file:
   reader = csv.DictReader(csv_file)
   for row in reader:
-      converted_row = convert_row(row)
-      if converted_row:
-        row_list.append(converted_row)
+      if(row['Country/Region'] != 'US'):
+          converted_row = convert_row(row)
+          if converted_row:
+            row_list.append(converted_row)
+try:
+    with open(args.US_CSV_FILE, 'r') as csv_file:
+      reader = csv.DictReader(csv_file)
+      for row in reader:
+          converted_row = convert_row_us(row)
+          if converted_row:
+            row_list.append(converted_row)
+except FileNotFoundError:
+    print('US DATA FILE NOT FOUND')
 with open(args.JS_FILE, 'w') as json_file:
   json_file.write(args.JS_VAR + ' = ')
   json.dump(row_list, json_file);
